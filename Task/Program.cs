138d42ece174fadbc2ddf7c6c940c7c5d1ada1f1
Anna <annkrv80@yandex.ru>
2022-09-20 21:12:43 +0700

    Пользователь вводит число элементов массива и заполняет массив

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Программа, которая из имеющегося массива строк формирует массив из строк,
+длина которых меньше либо равна 3 символа.*/
+Console.Clear();
+Console.Write("Введите число эллементов массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[size];
+FillingArray(array);
+
+
+
+
+void FillingArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t");
+        string letter=Console.ReadLine()??"";
+
+        if(letter!="")
+        {
+            array[i] = letter;
+        }
+        else
+        {
+            Console.WriteLine ("Ошибка! Введите данные!");
+            break;
+        }
+    }
+}
